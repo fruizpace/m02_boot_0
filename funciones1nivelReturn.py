2bbdb72267f66e39aaf36a3e617a6b20bdb42633
Fiorella <fruizpace@gmail.com>
2020-10-31 23:43:05 +0100

    Funciones 1er nivel que devuelven funciones

---
+++ funciones1nivelReturn.py
@@ -0,0 +1,45 @@
+def maxi(*l): # l es un array
+    if len(l) == 0:
+        return 0
+    m = l[0]
+    for ix in range(1,len(l)):
+        if l[ix] > m:
+            m = l[ix]
+    
+    return m
+
+
+def mini(*l):
+    if len(l) == 0:
+        return 0
+    m = l[0]
+    for ix in range(1,len(l)):
+        if l[ix] < m:
+            m = l[ix]    
+    
+    return m
+
+
+
+def media(*l):
+    if len(l) == 0:
+        return 0
+    
+    suma = 0
+    for valor in l:
+        suma += valor
+    
+    return suma / len(l)
+
+
+funciones = {'max': maxi, 'min': mini, 'med': media}
+
+def returnF(nombre):
+    if nombre.lower() in funciones.keys():
+        return funciones[nombre]
+    
+    return None
+
+returnF('max')(1,3,-1,15,9) # invocamos una función de primer nivel que nos devuelve una funcion,
+#cual? la que la pedimos.. en este caso MAXI
+# y la le damos sus parámetros para que se ejecute.
